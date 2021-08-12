0fef991db90cbfe54dbdcab725015cff0e786ded
DiSon93 <sonnguyendi93@gmail.com>
2021-08-12 21:57:38 +0700

    blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -26,8 +26,8 @@
                   </NuxtLink>
                   <div class="content">
                     Nhà cấp 3 là loại nhà chịu lực rất tốt, đây cũng là sự lựa chọn hàng
-                    đầu của đa số gia đình hiện nay. Để hiểu rõ thêm loại công trình
-                    này, tham khảo ngay bài viết viết dưới đây của Bất Động Sản Việt!
+                    đầu của đa số gia đình hiện nay. Để hiểu rõ thêm loại công trình này,
+                    tham khảo ngay bài viết viết dưới đây của Bất Động Sản Việt!
                   </div>
                 </v-col>
               </v-row>
